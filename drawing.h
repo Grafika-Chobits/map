281f5293679441576d804bf6f9f29cf0fe245af9
akhfa <akhmadfakhoni@gmail.com>
2015-02-28 21:36:40 +0700

    Added modul drawing everything

---
+++ drawing.h
@@ -0,0 +1,12 @@
+#ifndef DRAWING_H
+#define DRAWING_H
+
+#include "rotasi.h"
+#include "plotting.h"
+#include "allstruct.h"
+#include <vector>
+
+void drawPeta(Frame *frame, Coord center, RGB color);
+void drawKapal(Frame *frm, Coord loc, RGB color);
+
+#endif
